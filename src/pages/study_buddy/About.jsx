cea1f6847c57262a415a254b42ee6d4429969bc1
Mohit Parihar <mohitparihar3356@gmail.com>
2025-07-01 16:08:17 +0530

    Update About.jsx

--- src/pages/study_buddy/About.jsx
+++ src/pages/study_buddy/About.jsx
@@ -1,6 +1,5 @@
 // src/components/AboutSection.jsx
 import React from 'react';
-import { FaUsers, FaBook, FaChalkboardTeacher } from 'react-icons/fa';
 
 const About = () => (
   <section
@@ -20,7 +19,6 @@ const About = () => (
 
       <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
         <div className="p-6 bg-white rounded-lg shadow-lg">
-          <FaUsers className="text-indigo-500 mx-auto mb-4" size={48} />
           <h3 className="text-2xl font-semibold text-indigo-800 mb-2">
             Genuine Connections
           </h3>
@@ -31,7 +29,6 @@ const About = () => (
         </div>
 
         <div className="p-6 bg-white rounded-lg shadow-lg">
-          <FaBook className="text-indigo-500 mx-auto mb-4" size={48} />
           <h3 className="text-2xl font-semibold text-indigo-800 mb-2">
             Study Partner Matching
           </h3>
@@ -42,10 +39,7 @@ const About = () => (
         </div>
 
         <div className="p-6 bg-white rounded-lg shadow-lg">
-          <FaChalkboardTeacher
-            className="text-indigo-500 mx-auto mb-4"
-            size={48}
-          />
+     
           <h3 className="text-2xl font-semibold text-indigo-800 mb-2">
             Campus-First Design
           </h3>
